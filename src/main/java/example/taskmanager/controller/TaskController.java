f7977dd2c2a4c56969416dd3eb75431eca85707d
Bartosz Rogala <bartoszrogala23@gmail.com>
2024-08-08 11:48:40 +0200

    working structure for task manager

---
+++ src/main/java/example/taskmanager/controller/TaskController.java
@@ -0,0 +1,62 @@
+package example.taskmanager.controller;
+
+import example.taskmanager.TaskRepository;
+import example.taskmanager.model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Optional<Task> task = taskRepository.findById(id);
+        return task
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound()
+                        .build());
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            Task taskToUpdate = task.get();
+            taskToUpdate.setTitle(taskDetails.getTitle());
+            taskToUpdate.setDescription(taskDetails.getDescription());
+            taskToUpdate.setCompleted(taskToUpdate.isCompleted());
+            Task updatedTask = taskRepository.save(taskToUpdate);
+            return ResponseEntity.ok(updatedTask);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        if (taskRepository.existsById(id)) {
+            taskRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
