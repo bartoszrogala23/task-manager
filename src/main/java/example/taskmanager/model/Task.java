17bd8a222d052ca924175f748630c7c1f9400a20
Bartosz Rogala <bartoszrogala23@gmail.com>
2024-08-08 12:12:29 +0200

    add @notempty at title and default value for completed

--- src/main/java/example/taskmanager/model/Task.java
+++ src/main/java/example/taskmanager/model/Task.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,7 +17,8 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "Title is required")
     private String title;
     private String description;
-    private boolean completed;
+    private boolean completed = false;
 }
